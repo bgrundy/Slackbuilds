9867025948cc156f53b298004b2793c06360a2cb
bgrundy <bgrundy@gmail.com>
2016-08-16 15:49:43 -0400

    Updated checkmd5 script for use with sbotools

--- checkmd5.sh
+++ checkmd5.sh
@@ -17,8 +17,8 @@
 
 MD5FILE=CurrentMD5list.txt
 STATUSFILE=SBo_Version_Status.txt
-#SBOREPODIR=/usr/sbo/
-SBOREPODIR=/var/lib/sbopkg/SBo
+SBOREPODIR=/usr/sbo/repo
+#SBOREPODIR=/var/lib/sbopkg/SBo
 GITDIR=`pwd`
 
 ##########################
