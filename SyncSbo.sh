d1d7a027833fba7ae63127ea8f359ab8c481fc3c
bgrundy <bgrundy@gmail.com>
2017-01-15 13:40:48 -0500

    comment update

--- SyncSbo.sh
+++ SyncSbo.sh
@@ -3,6 +3,8 @@
 # Quick dirty script to merge changes made by SBo Maintainers on my 
 # SlackBuilds
 
+# I'm sure I'll pay for the over-simplicity at some point...
+
 for dirs in `cat SBo_Version_Status.txt | awk '{print $1}' | grep -E -v ^"\*\*"\|^$`
 do
 echo "Syncing $dirs..."
