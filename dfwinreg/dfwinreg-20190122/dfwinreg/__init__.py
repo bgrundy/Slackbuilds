497cec9e795f069c657c5cf69c3e059ccfddbaf6
bgrundy <bgrundy@gmail.com>
2019-03-06 17:31:52 -0500

    SBo Update

--- dfwinreg/dfwinreg-20190122/dfwinreg/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""Digital Forensics Windows Registry (dfWinReg).
-
-dfWinReg, or Digital Forensics Windows Registry, is a Python module
-that provides read-only access to Windows Registry objects.
-"""
-
-__version__ = '20190122'
