39685add0313b743ae5bf828b42428f3b644db08
bgrundy <bgrundy@gmail.com>
2023-07-20 21:31:17 -0400

    Updated LibyalSource.sh

--- LibyalSource.sh
+++ LibyalSource.sh
@@ -2,19 +2,15 @@
 #
 #Transfer source tarball to ionos:linuxleo
 #and hash the source file
-#
-##  NOT YET WORKING - CAREFUL of REMOTE FILE DELETION!
 
 SOURCENAME=$(basename $1) 
+PRGNAME=$(echo $SOURCNAME | awk -F'-' '{print $1}')
 LOCAL_MD5=$(md5sum $1)
 
-echo "Removing previous source..."
-ssh -t ionos 'cd linuxleocom/Source && pwd && ls $2 && rm -f $2'
-echo 
 echo "Copying to remote (scp)..."
 scp $1 ionos:linuxleocom/Source/.
 echo "Hashing remote source..."
-REMOTE_MD5=$(ssh -t ionos 'cd linuxleocom/Source && md5sum $1')
+REMOTE_MD5=$(ssh -t ionos "cd linuxleocom/Source && md5sum $1")
 echo "Remote hash: $REMOTE_MD5"
 echo "Local hash: $LOCAL_MD5"
 
